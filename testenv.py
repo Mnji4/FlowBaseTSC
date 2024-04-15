9f918aaa7b06d208579e580be37a04266400a0dc
Mnji4 <Mnji4@github.com>
2024-04-15 23:31:20 +0800

    testenv.py

--- testenv.py
+++ testenv.py
@@ -6,11 +6,13 @@ def run():
     for i in range(7200//env.seconds_per_step):
         env.step(np.random.randint(0, 7, size=(len(env.agentlist))))
         if env.now_step == 7200:
-            for k,v in env.vehicle_last_dis.items():
-                if (v[0]!=v[1]):
-                    print(k,v)
+            for k,v in env.test_pass.items():
+                if(len(v)):
+                    print(k,v) 
             
             print(f"pass {env.passnum} catch {env.catchnum}")
+            print(f"pass {env.testpassnum} catch {env.testcatchnum}")
+            print(env.remain_new_total)
             # print({k:len(v) for k,v in env.semi_buffer.items()})
     quit()
     env = make_parallel_env('config/config_jinan.json',2,42)
